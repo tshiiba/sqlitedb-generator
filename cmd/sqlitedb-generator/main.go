69a70e97e0240326fee2571f53acf1f897d441aa
t-shiiba <t.shiiba0514@gmail.com>
2026-01-11 18:34:45 +0900

    cmd作成

---
+++ cmd/sqlitedb-generator/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// This is a placeholder for the actual implementation of the sqlitedb-generator command.
+	fmt.Println("sqlitedb-generator command executed")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tshiiba/sqlitedb-generator
+
+go 1.25.5
